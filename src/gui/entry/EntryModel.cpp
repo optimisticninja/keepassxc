bcb8e0daa444b2aedf9eefcdc8a19c01f8bcc908
Jonathan White <support@dmapps.us>
2018-03-21 21:52:57 -0400

    Clean up Entry Model/View code

--- src/gui/entry/EntryModel.cpp
+++ src/gui/entry/EntryModel.cpp
@@ -72,10 +72,9 @@ void EntryModel::setGroup(Group* group)
     makeConnections(group);
 
     endResetModel();
-    emit switchedToListMode();
 }
 
-void EntryModel::setEntryList(const QList<Entry*>& entries)
+void EntryModel::setEntries(const QList<Entry*>& entries)
 {
     beginResetModel();
 
@@ -109,7 +108,6 @@ void EntryModel::setEntryList(const QList<Entry*>& entries)
     }
 
     endResetModel();
-    emit switchedToSearchMode();
 }
 
 int EntryModel::rowCount(const QModelIndex& parent) const
@@ -468,7 +466,7 @@ bool EntryModel::isUsernamesHidden() const
 /**
  * Set state of 'Hide Usernames' setting and signal change
  */
-void EntryModel::setUsernamesHidden(const bool hide)
+void EntryModel::setUsernamesHidden(bool hide)
 {
     m_hideUsernames = hide;
     emit usernamesHiddenChanged();
@@ -485,26 +483,10 @@ bool EntryModel::isPasswordsHidden() const
 /**
  * Set state of 'Hide Passwords' setting and signal change
  */
-void EntryModel::setPasswordsHidden(const bool hide)
+void EntryModel::setPasswordsHidden(bool hide)
 {
     m_hidePasswords = hide;
     emit passwordsHiddenChanged();
-}
-
-/**
- * Toggle state of 'Hide Usernames' setting
- */
-void EntryModel::toggleUsernamesHidden(const bool hide)
-{
-    setUsernamesHidden(hide);
-}
-
-/**
- * Toggle state of 'Hide Passwords' setting
- */
-void EntryModel::togglePasswordsHidden(const bool hide)
-{
-    setPasswordsHidden(hide);
 }
 
 void EntryModel::setPaperClipPixmap(const QPixmap& paperclip)
